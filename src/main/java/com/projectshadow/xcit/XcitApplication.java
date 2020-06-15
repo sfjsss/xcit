7ff347863f2d25febd89b54dfb789b623a78a81d
Tianyu Li <alandron06281990@gmail.com>
2020-06-14 22:46:19 -0700

    initial commit

---
+++ src/main/java/com/projectshadow/xcit/XcitApplication.java
@@ -0,0 +1,13 @@
+package com.projectshadow.xcit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectshadow/xcit/XcitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectshadow.xcit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
