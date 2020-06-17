843790caa14bf751b48715cbf6294b9e49c04265
Tianyu Li <alandron06281990@gmail.com>
2020-06-17 01:13:07 -0700

    done, next work on setup backend function to create a user

---
+++ src/main/java/com/projectshadow/xcit/entity/User.java
@@ -0,0 +1,44 @@
+package com.projectshadow.xcit.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Min(6)
+    private String password;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDate.now();
+    }
+}
