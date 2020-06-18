33120072570970e26d14dbed11219568847cc0f9
Tianyu Li <alandron06281990@gmail.com>
2020-06-18 00:14:45 -0700

    next work on creating controller

---
+++ src/main/java/com/projectshadow/xcit/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.projectshadow.xcit.repository;
+
+import com.projectshadow.xcit.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
