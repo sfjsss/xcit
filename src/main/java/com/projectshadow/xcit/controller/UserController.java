5aaa88026e4e6bebbc896e806d4c80d7aa0a3726
Tianyu Li <alandron06281990@gmail.com>
2020-06-19 00:04:28 -0700

    done, next check email uniqueness and handle exception

---
+++ src/main/java/com/projectshadow/xcit/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.projectshadow.xcit.controller;
+
+import com.projectshadow.xcit.entity.User;
+import com.projectshadow.xcit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public User registerUser(@RequestBody User user) {
+
+        user.setId(0);
+        userService.save(user);
+
+        return user;
+    }
+}
--- src/main/java/com/projectshadow/xcit/entity/User.java
+++ src/main/java/com/projectshadow/xcit/entity/User.java
@@ -1,19 +1,15 @@
 package com.projectshadow.xcit.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.Objects;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table(name="users")
 public class User {
@@ -31,15 +27,94 @@ public class User {
     private String email;
 
     @NotNull
-    @Min(6)
+    @Size(min = 6)
     private String password;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(updatable = false)
     private LocalDate createdAt;
 
+    public User() {
+    }
+
+    public User(long id, @NotNull String name, @Email @NotNull String email, @NotNull @Min(6) String password, LocalDate createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDate.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(createdAt, user.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, createdAt);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
     }
 }
