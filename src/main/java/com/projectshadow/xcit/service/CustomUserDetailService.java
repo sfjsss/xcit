1255cdc2fda5cc18f8afaa53b9f8439c9cf3b873
Tianyu Li <alandron06281990@gmail.com>
2020-06-23 23:30:59 -0700

    setup security config, next setup jwt

---
+++ src/main/java/com/projectshadow/xcit/service/CustomUserDetailService.java
@@ -0,0 +1,28 @@
+package com.projectshadow.xcit.service;
+
+import com.projectshadow.xcit.entity.User;
+import com.projectshadow.xcit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
