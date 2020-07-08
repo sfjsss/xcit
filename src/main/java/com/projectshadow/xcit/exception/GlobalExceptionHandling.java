0694f235bb3d49288fa2d68be3c6a4e4a368714e
Tianyu Li <alandron06281990@gmail.com>
2020-07-07 18:24:42 -0700

    setup handler, next work on send back response

--- src/main/java/com/projectshadow/xcit/exception/GlobalExceptionHandling.java
+++ src/main/java/com/projectshadow/xcit/exception/GlobalExceptionHandling.java
@@ -1,16 +1,20 @@
 package com.projectshadow.xcit.exception;
 
 import com.projectshadow.xcit.model.ErrorResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandling {
+public class GlobalExceptionHandling extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleDuplicateEmailException(DuplicateEmailException dex) {
@@ -32,5 +36,13 @@ public class GlobalExceptionHandling {
         ErrorResponse uer = new ErrorResponse(401, messages);
 
         return new ResponseEntity<>(uer, HttpStatus.UNAUTHORIZED);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        System.out.println(ex.getBindingResult().toString());
+
+        return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
     }
 }
