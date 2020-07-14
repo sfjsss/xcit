41326fd4b0771e6546fa9ee969cd71f24444255e
Tianyu Li <alandron06281990@gmail.com>
2020-07-14 15:52:15 -0700

    done, next work on throw userNotFoundException

---
+++ src/main/java/com/projectshadow/xcit/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.projectshadow.xcit.controller;
+
+import com.projectshadow.xcit.entity.User;
+import com.projectshadow.xcit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        User user = userService.findUserByEmail(email);
+
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
