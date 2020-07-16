2ded4fe708c21245987f4acc851bef0577a74506
Tianyu Li <alandron06281990@gmail.com>
2020-07-16 11:21:28 -0700

    done, next work on next UI feature

--- src/main/java/com/projectshadow/xcit/controller/UserController.java
+++ src/main/java/com/projectshadow/xcit/controller/UserController.java
@@ -6,10 +6,9 @@ import com.projectshadow.xcit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/public/users")
@@ -31,5 +30,11 @@ public class UserController {
         }
 
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<User>> getAllUserExceptThisEmail(@RequestParam("excludeEmail") String email) {
+        List<User> users = userService.findAllUserWithEmailExcept(email);
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
--- src/main/java/com/projectshadow/xcit/repository/UserRepository.java
+++ src/main/java/com/projectshadow/xcit/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.projectshadow.xcit.repository;
 import com.projectshadow.xcit.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+    List<User> findAllByEmailNot(String email);
 }
