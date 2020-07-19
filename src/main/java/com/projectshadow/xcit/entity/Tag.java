bbd41a52a47bf5bb26cec9a762b4ed67546920e4
Tianyu Li <alandron06281990@gmail.com>
2020-07-19 13:09:11 -0700

    done

---
+++ src/main/java/com/projectshadow/xcit/entity/Tag.java
@@ -0,0 +1,101 @@
+package com.projectshadow.xcit.entity;
+
+import com.projectshadow.xcit.model.TagCategory;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name="tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private TagCategory category;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    public Tag() {
+    }
+
+    public Tag(long id, @NotNull String name, @NotNull TagCategory category, LocalDate createdAt) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TagCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(TagCategory category) {
+        this.category = category;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDate.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return id == tag.id &&
+                Objects.equals(name, tag.name) &&
+                category == tag.category &&
+                Objects.equals(createdAt, tag.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, category, createdAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
